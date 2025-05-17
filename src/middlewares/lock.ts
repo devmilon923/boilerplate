bb6e615746c55838a1b95ba6424d6a88f742d5c5
devmilon923 <dev.milon923@gmail.com>
2025-05-17 21:01:32 +0600

    improved

--- src/middlewares/lock.ts
+++ src/middlewares/lock.ts
@@ -3,12 +3,12 @@ import ApiError from "../errors/ApiError";
 import { IUser } from "../modules/user/user.interface";
 
 export const validateUserLockStatus = async (user: IUser) => {
-  if (user?.blockStatus !== null) {
+  if (user?.blockStatus === null || user?.blockStatus <= new Date()) {
+    return true;
+  } else {
     throw new ApiError(
       httpStatus.BAD_REQUEST,
       "Your account is temporarily blocked"
     );
-  } else {
-    return true;
   }
 };
