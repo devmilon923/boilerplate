bbe63d1d8f9db386d7294935278ef9689900262f
devmilon <dev.milon923@gmail.com>
2025-10-28 18:18:48 +0600

    Remove dist folder from version control

--- dist/config/role.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERole = void 0;
-exports.ERole = ["admin", "user"];
-//# sourceMappingURL=role.js.map
--- dist/modules/user/user.interface.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.MoodEnum = void 0;
-exports.MoodEnum = ["Peaceful", "Grateful", "Hopeful", "Lonely", "Sad"];
-//# sourceMappingURL=user.interface.js.map
